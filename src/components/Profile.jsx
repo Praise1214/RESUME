6fe73b4651e97ca4b2cedbdf220cf408875f0f77
Praise1214 <agapepraiz@gmail.com>
2025-10-29 11:30:52 +0100

    change the styling of the profile section

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,23 +6,22 @@ const Profile = () => {
   return (
     <>
       <div className="relative">
-        <div className="absolute lg:w-[500px] lg:h-[500px] bg-[#2D2E39] rounded-full lg:-top-60 lg:-left-60 sm:w-[300px] sm:h-[300px] sm:-top-30 sm:-left-20">
+        <div className="absolute w-[300px] h-[300px] bg-[#2D2E39] rounded-full -top-30 -left-20 ">
         </div>
 
-        <div className="absolute sm:w-[200px] sm:h-[200px] border border-white rounded-full  sm:-top-25 sm:-left-22 lg:w-[400px] lg:h-[400px] lg:-top-60 lg:-left-52">
-        </div>
+        <div className=" absolute w-[200px] h-[200px] border border-white rounded-full  -top-25 -left-22">
 
-        <div className="absolute sm:w-[200px] sm:h-[200px] rounded-full sm:top-40 left-25 border-6 border-[#7e6557] lg:w-[300px] lg:h-[300px]">
-          <img 
-            src="/images/Praise.png"
-            alt="profile-image"
-            className="object-cover object-[center_10%] rounded-full w-full h-full top-50"
-          />
-        </div>
+          <div className="absolute w-[250px] h-[250px] rounded-full top-45 left-40 border-6 border-[#7e6557] ">
+            <img 
+              src="/images/Praise.png"
+              alt="profile-image"
+              className="object-cover object-[center_10%] rounded-full w-full h-full top-50"
+            />
+          </div>
 
-            <div className="absolute w-[300px] h-[300px] bg-[#ae9986] rounded-full top-30 left-195 ">
+            <div className="absolute w-[300px] h-[300px] bg-[#ae9986] rounded-full top-30 left-19 ">
             </div>
-        
+        </div>
 
 
       </div>
